eb08b0b0f1a31f9ce3c27088f4682b078fd33456
imane011989 <168775047+imane011989@users.noreply.github.com>
2024-05-12 13:07:10 +0200

    Update test_uni.py

--- test_uni.py
+++ test_uni.py
@@ -57,4 +57,6 @@ def test_shap_analysis():
     assert "SK_ID_CURR" in response.json()[0]
     assert "Feature Names and SHAP Values" in response.json()[0]
     assert isinstance(response.json()[0]["Feature Names and SHAP Values"], dict)
+    assert isinstance(response.json()[0]["Feature Names and SHAP Values"].values(), list)
+
 
