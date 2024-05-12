b2551482d14b2fe5948d0fb9db9e3f11e8819b22
imane011989 <168775047+imane011989@users.noreply.github.com>
2024-05-12 12:59:07 +0200

    Update test_uni.py

--- test_uni.py
+++ test_uni.py
@@ -3,137 +3,57 @@ import pytest
 from fastapi.testclient import TestClient
 from app import app
 
+# Créer un client de test pour l'application FastAPI
 client = TestClient(app)
 
-# Test de la route "/predict/"
+# Définir les données de test pour chaque endpoint
+test_input_data = {
+    "SK_ID_CURR": 12345,
+    "DAYS_EMPLOYED": -2000,
+    "DAYS_BIRTH": -15000,
+    "EXT_SOURCE_3": 0.7,
+    "DAYS_ID_PUBLISH": -1000,
+    "CODE_GENDER": 0,
+    "FLAG_OWN_CAR": 1,
+    "EXT_SOURCE_2": 0.6,
+    "EXT_SOURCE_1": 0.5,
+    "NAME_EDUCATION_TYPE_Highereducation": 1,
+    "NAME_CONTRACT_TYPE_Cashloans": 0,
+    "HOUR_APPR_PROCESS_START": 10,
+    "NAME_FAMILY_STATUS_Married": 1,
+    "FLAG_PHONE": 1,
+    "AMT_INCOME_TOTAL": 50000,
+    "AMT_CREDIT": 200000,
+    "DAYS_REGISTRATION": -2000,
+    "INCOME_CREDIT_PERC": 0.25,
+    "FLAG_DOCUMENT_3": 1,
+    "EMERGENCYSTATE_MODE_No": 1,
+    "WALLSMATERIAL_MODE_Panel": 1
+}
+
+# Tester la route d'accueil
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to your FastAPI application!"}
+
+# Tester la route de prédiction
 def test_predict():
-    # Entrée de test
-    test_input = {
-          "SK_ID_CURR": 123,
-            "DAYS_EMPLOYED": 1000,
-            "DAYS_BIRTH": 12000,
-            "EXT_SOURCE_3": 0.7,
-            "DAYS_ID_PUBLISH": 2000,
-            "CODE_GENDER": 1,
-            "FLAG_OWN_CAR": 0,
-            "EXT_SOURCE_2": 0.6,
-            "EXT_SOURCE_1": 0.5,
-            "NAME_EDUCATION_TYPE_Highereducation": 1,
-            "NAME_CONTRACT_TYPE_Cashloans": 1,
-            "HOUR_APPR_PROCESS_START": 10,
-            "NAME_FAMILY_STATUS_Married": 1,
-            "FLAG_PHONE": 0,
-            "AMT_INCOME_TOTAL": 100000,
-            "AMT_CREDIT": 200000,
-            "DAYS_REGISTRATION": 3000,
-            "INCOME_CREDIT_PERC": 0.5,
-            "FLAG_DOCUMENT_3": 1,
-            "EMERGENCYSTATE_MODE_No": 1,
-            "WALLSMATERIAL_MODE_Panel": 1
-    }
-    response = client.post("/predict/", json=test_input)
+    response = client.post("/predict/", json=test_input_data)
     assert response.status_code == 200
-    data = response.json()
-    assert "SK_ID_CURR" in data
-    assert "prediction" in data
-    # Vérifie que SK_ID_CURR dans la réponse correspond à celui de l'entrée
-    assert data["SK_ID_CURR"] == test_input["SK_ID_CURR"]
-    # Vérifie que la prédiction est soit 0 ou 1
-    assert data["prediction"] in [0, 1]
+    assert "SK_ID_CURR" in response.json()
+    assert "prediction" in response.json()
 
-# Test de la route "/predict_proba/"
+# Tester la route de prédiction de probabilités
 def test_predict_proba():
-    # Entrée de test avec plusieurs enregistrements
-    test_input = [
-        {
-              "SK_ID_CURR": 123,
-            "DAYS_EMPLOYED": 1000,
-            "DAYS_BIRTH": 12000,
-            "EXT_SOURCE_3": 0.7,
-            "DAYS_ID_PUBLISH": 2000,
-            "CODE_GENDER": 1,
-            "FLAG_OWN_CAR": 0,
-            "EXT_SOURCE_2": 0.6,
-            "EXT_SOURCE_1": 0.5,
-            "NAME_EDUCATION_TYPE_Highereducation": 1,
-            "NAME_CONTRACT_TYPE_Cashloans": 1,
-            "HOUR_APPR_PROCESS_START": 10,
-            "NAME_FAMILY_STATUS_Married": 1,
-            "FLAG_PHONE": 0,
-            "AMT_INCOME_TOTAL": 100000,
-            "AMT_CREDIT": 200000,
-            "DAYS_REGISTRATION": 3000,
-            "INCOME_CREDIT_PERC": 0.5,
-            "FLAG_DOCUMENT_3": 1,
-            "EMERGENCYSTATE_MODE_No": 1,
-            "WALLSMATERIAL_MODE_Panel": 1
-        },
-        {
-             "SK_ID_CURR": 123,
-            "DAYS_EMPLOYED": 1000,
-            "DAYS_BIRTH": 12000,
-            "EXT_SOURCE_3": 0.7,
-            "DAYS_ID_PUBLISH": 2000,
-            "CODE_GENDER": 1,
-            "FLAG_OWN_CAR": 0,
-            "EXT_SOURCE_2": 0.6,
-            "EXT_SOURCE_1": 0.5,
-            "NAME_EDUCATION_TYPE_Highereducation": 1,
-            "NAME_CONTRACT_TYPE_Cashloans": 1,
-            "HOUR_APPR_PROCESS_START": 10,
-            "NAME_FAMILY_STATUS_Married": 1,
-            "FLAG_PHONE": 0,
-            "AMT_INCOME_TOTAL": 100000,
-            "AMT_CREDIT": 200000,
-            "DAYS_REGISTRATION": 3000,
-            "INCOME_CREDIT_PERC": 0.5,
-            "FLAG_DOCUMENT_3": 1,
-            "EMERGENCYSTATE_MODE_No": 1,
-            "WALLSMATERIAL_MODE_Panel": 1
-        }
-    ]
-    response = client.post("/predict_proba/", json=test_input)
+    response = client.post("/predict_proba/", json=[test_input_data])
     assert response.status_code == 200
-    data = response.json()
-    assert "SK_ID_CURR" in data
-    assert "predicted_proba" in data
-    assert len(data["SK_ID_CURR"]) == len(test_input)
-    assert len(data["predicted_proba"]) == len(test_input)
+    assert "SK_ID_CURR" in response.json()
+    assert "predicted_proba" in response.json()
 
-# Test de la route "/shap/"
+# Tester la route d'analyse SHAP
 def test_shap_analysis():
-    # Entrée de test avec un seul enregistrement
-    test_input = [
-        {
-            "SK_ID_CURR": 123,
-            "DAYS_EMPLOYED": 1000,
-            "DAYS_BIRTH": 12000,
-            "EXT_SOURCE_3": 0.7,
-            "DAYS_ID_PUBLISH": 2000,
-            "CODE_GENDER": 1,
-            "FLAG_OWN_CAR": 0,
-            "EXT_SOURCE_2": 0.6,
-            "EXT_SOURCE_1": 0.5,
-            "NAME_EDUCATION_TYPE_Highereducation": 1,
-            "NAME_CONTRACT_TYPE_Cashloans": 1,
-            "HOUR_APPR_PROCESS_START": 10,
-            "NAME_FAMILY_STATUS_Married": 1,
-            "FLAG_PHONE": 0,
-            "AMT_INCOME_TOTAL": 100000,
-            "AMT_CREDIT": 200000,
-            "DAYS_REGISTRATION": 3000,
-            "INCOME_CREDIT_PERC": 0.5,
-            "FLAG_DOCUMENT_3": 1,
-            "EMERGENCYSTATE_MODE_No": 1,
-            "WALLSMATERIAL_MODE_Panel": 1
-        }
-    ]
-    response = client.post("/shap/", json=test_input)
+    response = client.post("/shap/", json=[test_input_data])
     assert response.status_code == 200
-    data = response.json()
-    # Vérifie que les valeurs SHAP sont présentes dans la réponse
-    assert "SK_ID_CURR" in data
-    assert "shap_values" in data
-    # Vérifie que les valeurs SHAP sont une liste non vide
-    assert isinstance(data["shap_values"], list)
-    assert len(data["shap_values"]) > 0
+    assert "SK_ID_CURR" in response.text
+    assert "Feature Names and SHAP Values" in response.text
